fd71043a85a7687864715ba1117ccdc440c11ad7
Richard Pereira do Nascimento <richard.nascimento@ilia.digital>
2022-04-24 19:38:33 -0300

    :wrench: Configure project

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(HelloWorld);
 };
